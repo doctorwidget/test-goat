7d61dbe34e4b5951df1bb8d1b104b72553f6b0b9
Scott N Fitz <doctorwidget@gmail.com>
2014-12-29 09:20:49 -0800

    Refactored home page view to use a template. Added pytest.ini file so we can use pytest as our test runner.

--- goat/settings.py
+++ goat/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'goat.apps.lists',
 )
 
 MIDDLEWARE_CLASSES = (
--- test/test_functional.py
+++ test/test_functional.py
@@ -2,6 +2,7 @@
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -19,25 +20,42 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         # she notices the page title and header mention to-do lists
-        self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        todo = 'To-Do'
+        self.assertIn(todo, self.browser.title)
+        header = self.browser.find_element_by_class_name('h1').text
+        self.assertIn(todo, header)
 
         # she is invited to enter a to-do item somewhere on the page
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # she types "buy peacock feathers" into a text box
+        inputbox.send_keys('Buy peacock feathers')
 
-        # when she hits enter, the page updates, and now the page lists 1. Buy peacock
-        # feathers as an item in a to-do list
+        # when she hits enter, the page updates, and now the page lists
+        # 1. Buy peacock feathers as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
 
-        # there is still text inviting her to add another item. She enters "use peacock
-        # feathers to make a fly" (she is very methodical)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        expected = '1: Buy peacock feathers'
+        self.assertTrue(
+            any(row.text == expected for row in rows)
+        )
 
-        # The page updates agian, and now shows both items on her list. 
+        # there is still text inviting her to add another item. She enters
+        # "use peacock feathers to make a fly" (she is very methodical)
+        self.fail('Finish the test! It still needs a second TODO item')
+
+        # The page updates again, and now shows both items on her list.
 
 
-        # Edith wonders whether or not the site will remember her list. Then she sees
-        # that the site has generated a unique URL for her. There is now some
-        # explanatory text to that effect
+        # Edith wonders whether or not the site will remember her list. Then
+        # she sees that the site has generated a unique URL for her. There is
+        #  now some explanatory text to that effect
 
 
         # She visits that URL and sees that her to-do list is still there.
