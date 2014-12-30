9ff762d0b5b032886a70c6e256d9248b8f36590e
Scott N Fitz <doctorwidget@gmail.com>
2014-12-29 17:11:29 -0800

    Refactored all urls related to the lists app into a dedicated urls file.

---
+++ goat/apps/lists/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, url
+
+urlpatterns = patterns('',
+    # comes in with /lists/ prefixed
+    url(r'^(\d+)/$',
+        'goat.apps.lists.views.view_list',
+        name = 'view_list'),
+    url(r'^(\d+)/add_item$',
+        'goat.apps.lists.views.add_item',
+        name = 'add_item'),
+    url(r'^new$',
+        'goat.apps.lists.views.new_list',
+        name = 'new_list'),
+
+)
