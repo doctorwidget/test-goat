9a76245d3d58710e27743e5b4344ad2b4f83442c
Scott N Fitz <doctorwidget@gmail.com>
2014-12-29 17:06:51 -0800

    Finished refactoring to a sane handling of lists and items in views and urls

--- goat/apps/lists/tests/test_views.py
+++ goat/apps/lists/tests/test_views.py
@@ -23,19 +23,25 @@ class HomePageTest(TestCase):
 class ListViewTest(TestCase):
 
     def test_uses_list_template(self):
-        response = self.client.get('/lists/uberlist/')
+        list_ = List.objects.create()
+        response = self.client.get('/lists/%d/' % (list_.id))
         self.assertTemplateUsed(response, 'lists/list.html')
 
-    def test_displays_all_list_items(self):
+    def test_displays_only_items_for_that_list(self):
         alist = List.objects.create()
         Item.objects.create(text='itemey 1', list=alist)
         Item.objects.create(text='itemey 2', list=alist)
 
-        response = self.client.get('/lists/uberlist/')
+        blist = List.objects.create()
+        Item.objects.create(text='spam 1', list=blist)
+        Item.objects.create(text='eggs 2', list=blist)
+
+        response = self.client.get('/lists/%d/' % (alist.id,))
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
-
+        self.assertNotContains(response, 'spam 1')
+        self.assertNotContains(response, 'eggs 2')
 
 class NewListTest(TestCase):
 
@@ -56,6 +62,40 @@ class NewListTest(TestCase):
             data={'new_item_text': 'whatever'}
         )
 
-        self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, '/lists/uberlist/')
+        new_list = List.objects.first()
+        self.assertRedirects(response, '/lists/%d/' % (new_list.id,))
 
+
+class NewItemTestCase(TestCase):
+
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            '/lists/%d/add_item' % (correct_list.id,),
+            data={'new_item_text': 'A new item for an existing list'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+        self.assertNotEqual(other_list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            '/lists/%d/add_item' % (correct_list.id,),
+            data={'new_item_text': 'A new item for an existing list'}
+        )
+
+        self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
+
+    def test_passes_correct_list_to_template(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get('/lists/%d/' % (correct_list.id,))
+        self.assertEqual(response.context['list'], correct_list)
--- goat/apps/lists/views.py
+++ goat/apps/lists/views.py
@@ -6,9 +6,9 @@ from goat.apps.lists.models import Item, List
 def home_page(request):
     return render(request, 'lists/home.html')
 
-def view_list(request):
-    items = Item.objects.all()
-    return render(request, 'lists/list.html', {'items': items})
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    return render(request, 'lists/list.html', {'list': list_})
 
 def new_list(request):
     list_ = List.objects.create()
@@ -16,4 +16,12 @@ def new_list(request):
         text=request.POST['new_item_text'],
         list=list_
     )
-    return redirect('/lists/uberlist/')
+    return redirect('/lists/%d/' % (list_.id,))
+
+def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(
+        text=request.POST['new_item_text'],
+        list=list_
+    )
+    return redirect('/lists/%d/' % (list_.id,))
