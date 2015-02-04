a89b2ea703c86c5ff35488fe545f8f17aefbefea
Scott N Fitz <doctorwidget@gmail.com>
2015-02-03 16:46:12 -0800

    Added Sinon for Javascript mocking along with related unit tests.

--- goat/apps/accounts/static/accounts/accounts.js
+++ goat/apps/accounts/static/accounts/accounts.js
@@ -1,14 +1,35 @@
 /** mock object for use with Qunit testing **/
 /*global $ */
 
-var initialize = function (navigator) {
+var initialize = function (navigator, user, token, urls) {
    $('#id_login').on('click', function() {
          navigator.id.request();
    });
+
+    navigator.id.watch({
+        loggedInUser: user,
+        onlogin: function(assertion) {
+            $.post(
+                urls.login,
+                    {
+                        assertion: assertion,
+                        csrfmiddlewaretoken: token
+                    })
+                .done(function () {
+                  window.location.reload();
+                })
+                .fail(function () {
+                    navigator.id.logout();
+                });
+        },
+        onlogout: function() {}
+    });
+
 };
 
 window.Goat = {
    Accounts: {
        initialize: initialize
    }
-}
+};
+
